5e34378665c292e025aa1976fc71f421f52afcec
Jiří Klimeš <jklimes@redhat.com>
2012-12-17 16:57:12 +0100

    core: clean up a device when unmanaging it

--- src/nm-device.c
+++ src/nm-device.c
@@ -4836,6 +4836,9 @@ nm_device_state_changed (NMDevice *device,
 		nm_device_set_firmware_missing (device, FALSE);
 		if (old_state > NM_DEVICE_STATE_UNMANAGED && reason == NM_DEVICE_STATE_REASON_SLEEPING)
 			nm_device_take_down (device, TRUE, reason);
+
+		/* Clean up device when going to unmanaged state, like stopping DHCP, etc. */
+		device_cleanups (device);
 		break;
 	case NM_DEVICE_STATE_UNAVAILABLE:
 		if (old_state == NM_DEVICE_STATE_UNMANAGED || priv->firmware_missing) {
