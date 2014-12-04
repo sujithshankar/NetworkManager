sujithshankar/NetworkManager

commit d37b7bed305dbed7b5351038850ef6bbb7ed9c59

core: let kernel add IPv6LL address when method=ignore (rh #1132938)
The IPv6LL address handling in userspace patches failed to handle the
case where the IPv6 method was 'ignore'.  Previously the kernel would
usually add the IPv6LL address itself, but if NM has turned off kernel
IPv6LL then obviously this wouldn't happen.  So when the method is
'ignore', turn off userspace IPv6LL handling and bounce disable_ipv6
to make the kernel add the IPv6LL address if it wants to.

src/devices/nm-device.c
@@ -3988,10 +3988,24 @@ act_stage3_ip6_config_start (NMDevice *self,
 	priv->dhcp6_mode = NM_RDISC_DHCP_LEVEL_NONE;
 
 	if (strcmp (method, NM_SETTING_IP6_CONFIG_METHOD_IGNORE) == 0) {
-		if (!priv->master)
+		if (!priv->master) {
+			/* When activating an IPv6 'ignore' connection we need to revert back
+			 * to kernel IPv6LL, but the kernel won't actually assign an address
+			 * to the interface until disable_ipv6 is bounced.
+			 */
+			set_nm_ipv6ll (self, FALSE);
+			nm_device_ipv6_sysctl_set (self, "disable_ipv6", "1");
 			restore_ip6_properties (self);
+		}
 		return NM_ACT_STAGE_RETURN_STOP;
 	}
+
+	/* Any method past this point requires an IPv6LL address. Use NM-controlled
+	 * IPv6LL if this is not an assumed connection, since assumed connections
+	 * will already have IPv6 set up.
+	 */
+	if (!nm_device_uses_generated_assumed_connection (self))
+		set_nm_ipv6ll (self, TRUE);
 
 	/* Re-enable IPv6 on the interface */
 	set_disable_ipv6 (self, "0");

commit 155d5bc5a156a180c242b772abaf8c458aa67007

core: don't bounce disable_ipv6 when assuming connections (rh #1170530)
Don't call set_nm_ipv6ll(self, TRUE) on any assumed connections since it
would bounce disable_ipv6, which would break IPv6 connectivity.
That is critical, for example, for installations via NFS.

Fixes: d37b7bed305dbed7b5351038850ef6bbb7ed9c59

https://bugzilla.redhat.com/show_bug.cgi?id=1170530
Related: https://bugzilla.redhat.com/show_bug.cgi?id=1052157

src/devices/nm-device.c
@@ -4390,7 +4390,7 @@ act_stage3_ip6_config_start (NMDevice *self,
 	 * IPv6LL if this is not an assumed connection, since assumed connections
 	 * will already have IPv6 set up.
 	 */
-	if (!nm_device_uses_generated_assumed_connection (self))
+	if (!nm_device_uses_assumed_connection (self))
 		set_nm_ipv6ll (self, TRUE);
 
 	/* Re-enable IPv6 on the interface */